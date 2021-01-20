df1bb75702d412c6dc1f88c1878d838f5cec6bfc
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-01-20 19:01:01 +0530

    refractor: imgui content fetch , fix: large response crash

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,6 @@ int main(int, char **)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
-    (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
                                                           //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
