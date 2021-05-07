27167b505cf57aa9de8b06034fe63c762856faeb
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-05-07 20:11:49 +0530

    fix: cmake variables

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int, char **)
 
 
     std::stringstream ss;
-    ss << "xP v" << XP_VERSION_MAJOR<<'.'<<XP_VERSION_MINOR;
+    ss << "xP v" << xP_VERSION_MAJOR<<'.'<<xP_VERSION_MINOR;
 
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
@@ -138,7 +138,7 @@ int main(int, char **)
     //IM_ASSERT(font != NULL);
 
     // Our state
-#ifdef XP_SHOW_IMGUI_DEMO_WINDOW
+#ifdef xP_SHOW_IMGUI_DEMO_WINDOW
     bool show_demo_window = false;
     show_demo_window = false;
 #endif
@@ -161,7 +161,7 @@ int main(int, char **)
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
         // if (show_demo_window)
 
-#ifdef XP_SHOW_IMGUI_DEMO_WINDOW
+#ifdef xP_SHOW_IMGUI_DEMO_WINDOW
         ImGui::ShowDemoWindow(&show_demo_window);
 #endif
         gui.render();
