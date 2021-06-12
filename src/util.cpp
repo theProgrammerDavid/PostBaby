c513b3ebe06c2b428b7a74c3ae4013f8baf7281b
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-06-12 21:13:11 +0530

    fix: absolute file path

--- src/util.cpp
+++ src/util.cpp
@@ -11,12 +11,12 @@ std::string absolutePath()
   wchar_t path[FILENAME_MAX] = {0};
   GetModuleFileNameW(nullptr, path, FILENAME_MAX);
   std::string tempString = std::filesystem::path(path).string();
-  return tempString.substr(0, tempString.length() - 6);
+  return tempString.substr(0, tempString.length() - (sizeof(PROJECT_NAME) + 3)); // PROJECT_NAME + ".exe"
 #else
   char path[FILENAME_MAX];
   ssize_t count = readlink("/proc/self/exe", path, FILENAME_MAX);
   std::string tempString = std::string(std::string(path, (count > 0) ? count : 0));
-  return tempString.substr(0, tempString.length() - 2);
+  return tempString.substr(0, tempString.length() - sizeof(PROJECT_NAME));
 #endif
 }
 
